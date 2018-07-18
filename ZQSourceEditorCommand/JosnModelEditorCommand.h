12a805bb43940bd47351a05a24ca3d22a097a158
郑泽钦 <zhengzeqin@addcn.com>
2018-07-18 17:54:50 +0800

    +++

--- ZQSourceEditorCommand/JosnModelEditorCommand.h
+++ ZQSourceEditorCommand/JosnModelEditorCommand.h
@@ -8,6 +8,11 @@
 
 #import <XcodeKit/XcodeKit.h>
 
+/*
+ 1.XCSourceEditorCommand 是另外一个核心概念，其实就是当插件被触发之后，你有机会在代理方法里面拦截到这个消息（XCSourceEditorCommandInvocation），做出处理之后将内容返回给 Xcode，仅此而已
+ 2.XCSourceEditorCommandInvocation 里面会存放一些 meta 数据，其中最重要的是 identifier 和 buffer，identifier 就是用来做区分的，buffer 则是整个插件中最最重要的概念，但是他其实也很简单。
+ 3.
+ */
 @interface JosnModelEditorCommand : NSObject <XCSourceEditorCommand>
 
 @end
--- ZQSourceEditorCommand/SourceEditorExtension.m
+++ ZQSourceEditorCommand/SourceEditorExtension.m
@@ -18,6 +18,7 @@
 */
 
 /*
+ 
 - (NSArray <NSDictionary <XCSourceEditorCommandDefinitionKey, id> *> *)commandDefinitions
 {
     // If your extension needs to return a collection of command definitions that differs from those in its Info.plist, implement this optional property getter.
